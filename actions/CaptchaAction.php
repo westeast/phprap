6e0979c3c6b64af46bc87ce430aa41b21155b3b8
勾国印 <gouguoyin@juzifenqi.com>
2018-05-28 00:46:37 +0800

    7、修复验证码刷新页面不会自动刷新的BUG

--- actions/CaptchaAction.php
+++ actions/CaptchaAction.php
@@ -2,18 +2,19 @@
 
 namespace app\actions;
 
+use Yii;
 use yii\web\Response;
 
 class CaptchaAction extends \yii\captcha\CaptchaAction
 {
 
     /**
-     * 默认验证码刷新页面不会自动刷新
+     * 修复验证码刷新页面不会自动刷新的BUG
      */
     public function run()
     {
         $this->setHttpHeaders();
-        \Yii::$app->response->format = Response::FORMAT_RAW;
+        Yii::$app->response->format = Response::FORMAT_RAW;
         return $this->renderImage($this->getVerifyCode(true));
     }
 
