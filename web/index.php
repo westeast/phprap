98a6997c3b35a473ae11f5c3f362f6e2af8a2d5a
勾国印 <gouguoyin@juzifenqi.com>
2018-06-27 09:34:47 +0800

    提交vendor

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 
 error_reporting(0);
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../vendor/autoload.php';
